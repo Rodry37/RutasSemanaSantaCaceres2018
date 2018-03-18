8e703697d4424bd138c4e987a65e5a98bb70791b
Rodri <rodrisolla@gmail.com>
2018-03-18 13:04:29 +0100

    Added string to strings.xml Added Toasts Tested JSON load

--- app/src/main/java/com/imovil/uo239737/rutassemanasantacaceres2018/MainActivity.java
+++ app/src/main/java/com/imovil/uo239737/rutassemanasantacaceres2018/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -28,7 +29,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getRoutesFromJSON();
+        if(isOnline()) {
+            loadRoutesFromJSON();
+        }
+        else{
+            CharSequence mens = getString(R.string.toast_no_network);
+            Toast t = Toast.makeText(getApplicationContext(), mens, Toast.LENGTH_SHORT);
+            t.show();
+        }
 
     }
 
@@ -39,8 +47,8 @@ public class MainActivity extends AppCompatActivity {
         return (networkInfo != null && networkInfo.isConnected());
     }
 
-    private ArrayList<Ruta> getRoutesFromJSON(){
-        ArrayList<Ruta> r = new ArrayList<>();
+    private void loadRoutesFromJSON(){
+        routes = new ArrayList<>();
         RequestQueue queue = Volley.newRequestQueue(this);
         JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -52,15 +60,29 @@ public class MainActivity extends AppCompatActivity {
                         Ruta r = new Ruta();
                         r.setUri(data.getJSONObject(i).getJSONObject("uri").getString("value"));
                         r.setNombre(data.getJSONObject(i).getJSONObject("rdfs_label").getString("value"));
-                        r.setFecha_salida(dateParser(data.getJSONObject(i).getJSONObject("time_at").getString("value")));
                         r.setLugar_salida(data.getJSONObject(i).getJSONObject("om_sitioDeSalida").getString("value"));
                         r.setLugar_llegada(data.getJSONObject(i).getJSONObject("om_sitioDeRecogida").getString("value"));
                         r.setDia(data.getJSONObject(i).getJSONObject("time_day").getString("value"));
                         r.setPasos_Asociados(getPasosJSON(data.getJSONObject(i).getJSONObject("pasosAsociados").getString("value")));
-                        setTrazadoJSON(data.getJSONObject(i).getJSONObject("").getString("value"), r);
+                        setTrazadoJSON(data.getJSONObject(i).getJSONObject("locn_geometry").getString("value"), r);
+
+                        //date has to be inside a try because some objects of the JSONArray do not have the 'time_at' value
+                        try{
+                            r.setFecha_salida(dateParser(data.getJSONObject(i).getJSONObject("time_at").getString("value")));
+                        }
+                        catch (Exception e) {
+                            r.setFecha_salida(new Date(0));
+                        };
+
+
+                        routes.add(r);
 
 
                     }
+
+                    CharSequence mens = getString(R.string.toast_load_ok);
+                    Toast t = Toast.makeText(getApplicationContext(), mens, Toast.LENGTH_SHORT);
+                    t.show();
                 }
                 catch(Exception e){
                     e.printStackTrace();
@@ -70,18 +92,20 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                CharSequence mens = getString(R.string.toast_load_fail);
+                Toast t = Toast.makeText(getApplicationContext(), mens, Toast.LENGTH_SHORT);
+                t.show();
 
             }
         });
         queue.add(req);
 
-        return r;
     }
 
     private void setTrazadoJSON(String value, Ruta r) {
 
         String[] puntosSTR = value.replace("[","").replace("]","").split(",");
-        for (int i = 0 ; i>puntosSTR.length ; i=i+2){
+        for (int i = 0 ; i<puntosSTR.length ; i=i+2){
             r.addTrazado(Double.parseDouble(puntosSTR[i+1]), Double.parseDouble(puntosSTR[i]));
         }
     }
