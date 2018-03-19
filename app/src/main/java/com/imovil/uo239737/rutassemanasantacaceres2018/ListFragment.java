ed73956d43509f658a4ce7680902f47b30748b1c
Rodri <rodrisolla@gmail.com>
2018-03-19 10:42:31 +0100

    Created Recycler View class, Fragment List class and layouts

---
+++ app/src/main/java/com/imovil/uo239737/rutassemanasantacaceres2018/ListFragment.java
@@ -0,0 +1,11 @@
+package com.imovil.uo239737.rutassemanasantacaceres2018;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Rodry on 19/03/2018.
+ */
+
+public class ListFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/imovil/uo239737/rutassemanasantacaceres2018/RecyclerViewAdapter.java
@@ -0,0 +1,50 @@
+package com.imovil.uo239737.rutassemanasantacaceres2018;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Rodry on 19/03/2018.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    public LayoutInflater inflater;
+
+    public RecyclerViewAdapter(Context context){
+        this.inflater = LayoutInflater.from(context);
+    }
+
+
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder
+            (ViewGroup viewGroup, int type) {
+
+        View view = inflater.inflate(R.layout.layout_row,viewGroup,false);
+        return new RecyclerViewAdapter.ViewHolder(view);
+    }
+
+
+    public void onBindViewHolder
+            (RecyclerViewAdapter.ViewHolder viewHolder, int i) {
+
+
+    }
+
+    public int getItemCount() {
+        return 0;
+    }
+
+    protected static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView NombreRuta;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+
+            NombreRuta = (TextView) itemView.findViewById(R.id.lbNombreRuta);
+        }
+    }
+}
